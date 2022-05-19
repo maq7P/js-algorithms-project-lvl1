a0ad78d823e066f1693133657ce7445959c5a451
maq7 <maxpet16@mail.ru>
2022-05-19 16:11:38 +0500

    [step 6]: have been added TF-IDF metric

--- src/index.js
+++ src/index.js
@@ -4,7 +4,50 @@ class BuildSearchEngine {
   constructor(docs) {
     this.docs = docs
   }
-  static invertIndexById(docs){
+
+  static calcTF_IDF(
+    termDocumentFrequency,
+    documentsSize,
+    collectionSize,
+    termCollectionFrequency,
+  ) {
+    const TF = termDocumentFrequency / collectionSize;
+    const IDF =  Math.log10(documentsSize / termCollectionFrequency);
+    return TF * IDF;
+    // Вторая формула
+    //(termDocumentFrequency / documentSize) * Math.log2(1 + collectionSize / termCollectionFrequency)
+  }
+
+  #calcTargetTF_IDF(builtIndex, targetArr){
+    const score = {};
+    let commonFrequency = 0;
+
+    for(let i = 0; i < targetArr.length; i++){
+      const word = targetArr[i].toLowerCase();
+      for(let docId in builtIndex[word]){
+        commonFrequency++;
+      }
+
+      if(!commonFrequency) return {}
+
+      for(let docId in builtIndex[word]){
+        let currentFrequency = builtIndex[word][docId];
+
+        const curScore = BuildSearchEngine.calcTF_IDF(
+          commonFrequency,
+          this.getSizeDocs(),
+          this.getSizeDoc(docId),
+          currentFrequency
+        );
+
+        score[docId] ? score[docId] += curScore : score[docId] = curScore;
+      }
+    }
+
+    return score
+  }
+
+  buildIndex(docs){
     const index = {};
 
     for(let i = 0; i < docs.length; i++){
@@ -16,34 +59,53 @@ class BuildSearchEngine {
       for(let i = 0; i < words.length; i++){
         const curWordInText = words[i].toLowerCase().replace(/[,.?!]/g, "");
 
-        index[curWordInText] && !index[curWordInText].includes(docId) && index[curWordInText].push(docId);
-        !index[curWordInText] && (index[curWordInText] = [docId]);
+        if(index[curWordInText]){
+          index[curWordInText][docId]
+            ? index[curWordInText][docId]++
+            : index[curWordInText][docId] = 1
+        } else {
+          index[curWordInText] = {[docId]: 1}
+        }
       }
     }
 
     return index
   }
 
+  getSizeDocs(){
+    return this.docs.length;
+  }
+
+  getSizeDoc(id){
+    return this.docs
+      .find(doc => doc.id === id)
+      .text.split(" ").length
+  }
+
+  getPopularDoc(objDocsWithTF_IDF){
+    return Object
+      .entries(objDocsWithTF_IDF)
+      .sort(([, ascore], [, bscore]) => bscore - ascore)
+      .map(([id]) => id);
+  }
+
   search(word){
-    const wordArr = word.split(" ");
+    if(!this.docs.length) return [];
 
-    const res = [];
-    for (let i = 0; i < wordArr.length; i++){
-      const word = wordArr[i].toLowerCase().replace(/[,.?!]/g, "");
-      const target = BuildSearchEngine.invertIndexById(this.docs)[word];
-      target && res.push(...target);
-    }
+    const buildIndex = this.buildIndex(docs);
+    const calculatedTargetTF_IDF = this.#calcTargetTF_IDF(buildIndex, word.split(" "));
 
-    return [...new Set(res)];
+    return this.getPopularDoc(calculatedTargetTF_IDF)
   }
 }
 
 const searchEngine = new BuildSearchEngine(docs); // поисковый движок запомнил документы
 
 // поиск по документам
-console.log(searchEngine.search('shoot kke')); // ['doc1', 'doc2']
+console.log(searchEngine.search('shoot'));
 
 const searchEngine2 =  new BuildSearchEngine([]); // Документы пусты
-console.log(searchEngine2.search('')); // []
+
+console.log(searchEngine2.search('shoot'));
 
 export default BuildSearchEngine;
--- src/moc.js
+++ src/moc.js
@@ -2,6 +2,6 @@
 // документ имеет два атрибута "id" и "text"
 const doc1 = {id: 'doc1', text: "I can't shoot straight unless I've had a pint!"};
 const doc2 = {id: 'doc2', text: "i'm Don't shoot shoot shoot that thing at me."};
-const doc3 = {id: 'doc3', text: "I'm your shooter."};
+const doc3 = {id: 'doc3', text: "I'm your shooter that shoot shoot. but but but but but but but but but but but but"};
 
 export const docs = [doc1, doc2, doc3];
